cde93534721d47addbe8821f957b4d3f9c7340d7
JaymZZZ <jimok82@gmail.com>
2015-08-21 14:23:06 -0500

    fixed rating values (openreviewscript compatibility)

---
+++ htdocs/application/config/cior.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * CIOpenReview
+ *
+ * An Open Source Review Site Script base on the abandoned OpenReviewScript
+ *
+ * @filename    cior.php
+ * @package        CIOpenReview
+ * @author        JaymZZZ
+ * @date    8/21/2015
+ * @time    2:16 PM
+ * @copyright           Copyright (c) 2014-2015 MindBrite.com , Portions Copyright (c) 2011-2012, OpenReviewScript.org
+ * @license        This file is part of CIOpenReview with portions used from OpenReviewScript - free software licensed under the GNU General Public License version 2 - http://OpenReviewScript.org/license
+ * @link        http://mindbrite.com/ciopenreview
+ */
+// ------------------------------------------------------------------------
+//
+/**    This file is part of CIOpenReview.
+ *
+ *    CIOpenReview is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    CIOpenReview is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with CIOpenReview.  If not, see <http://www.gnu.org/licenses/>.
+ */
